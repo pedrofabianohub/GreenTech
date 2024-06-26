7a5a8908c24bd29632f3c5194516d04c1318ec4c
pedrofabianohub <pedrofabiano421@gmail.com>
2024-06-26 17:03:30 -0300

    Setup Netlify deployment with serverless functions

--- js/backend/server.js
+++ js/backend/server.js
@@ -1,4 +1,7 @@
 const serverless = require('serverless-http');
 const app = require('./backend');
 
-module.exports.handler = serverless(app);
+module.exports.handler = async (event, context) => {
+  const result = await serverless(app)(event, context);
+  return result;
+};
