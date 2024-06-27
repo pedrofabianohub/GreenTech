cd7cb8c4d9efdcf67f0533d9c7bfda685ebc8193
pedrofabianohub <pedrofabiano421@gmail.com>
2024-06-27 14:49:29 -0300

    Testando backend com o Vercel

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const {
 } = require("@google/generative-ai");
 
 const app = express();
+app.use(express.json());
 app.use(cors());
-app.use(express.json()); // Middleware para parsing do corpo da requisição
+
 
 const port = process.env.PORT || 8000;
 
